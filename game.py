c7dd02e32228fafb35f17716766bd5e2d56f6559
Jim <jim@jam.sessionsnet.org>
2021-12-18 16:57:29 -0800

    add a game loop that ends when gameOver is true

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 import pygame
 from pygame.locals import *
 
+gameOver = False
+
 pygame.init()
 
-# gaame goes here
+# game loop goes here
+
+while not gameOver:
+    # play one step of the game, and test for game over
+
+    # (for now, there is no such test, so this is an endless loop)
+
+    # at the end of each loop, update the display
+    pygame.display.update()
 
 pygame.quit()
