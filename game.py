e1c008431c4fa60f55d384aa49295b9025d916c2
Jim <jim@jam.sessionsnet.org>
2021-12-21 11:28:40 -0800

    mostly game loop

---
+++ game.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import pygame
+from pygame.locals import *
+import sys, random
+
+pygame.init()
+
+FPS = 60
+FramePerSec  = pygame.time.Clock()
+
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+gameOver = False
+
+while not gameOver:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            gameOver = True
+        elif event.type == KEYDOWN:
+            if event.key - pygame.K_q:
+                gameOver = True
