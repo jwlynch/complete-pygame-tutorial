938833dd32a3cf8adb40dfe78335ef8e063f8d2b
Jim <jim@jam.sessionsnet.org>
2021-12-24 01:45:15 -0800

    add Enemy class, with methods

--- game.py
+++ game.py
@@ -19,6 +19,23 @@ DISPLAYSURF = pygame.display.set_mode((400, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image - pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        self.rect.move_ip(0, 10)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+        def draw(self, surface):
+            surface.blit(self.image, self.rect)
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
