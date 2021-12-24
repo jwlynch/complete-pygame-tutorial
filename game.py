e7f30bb820f6dec87fcfa5da7bc1081a831822e2
Jim <jim@jam.sessionsnet.org>
2021-12-24 01:44:04 -0800

    add Player class, with methods

--- game.py
+++ game.py
@@ -19,6 +19,33 @@ DISPLAYSURF = pygame.display.set_mode((400, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+
+        #if pressed_keys[K_UP]:
+            #self.rect.move_ip(0, -5)
+
+        #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(0, 5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
 gameOver = False
 
 while not gameOver:
